de9a575daaa75733d82808735bc4341e457f9e4b
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-10 21:55:23 -0400

    Update 80-sma-link.bash

--- root/etc/cont-init.d/80-sma-link.bash
+++ root/etc/cont-init.d/80-sma-link.bash
@@ -9,8 +9,12 @@ if [ ! -f "/config/sma/config/autoProcess.ini" ]; then
 	cp "/usr/local/sma/config/autoProcess.ini.sample" "/config/sma/config/autoProcess.ini" && \
 	chmod 0666 "/config/sma/config"/*
 fi
+if [ ! -f "/usr/local/sma/config/autoProcess.ini" ]; then
+	ln -s "/config/sma/config/autoProcess.ini" "/usr/local/sma/config/autoProcess.ini"
+fi
 
-ln -s "/config/sma/config/autoProcess.ini" "/usr/local/sma/config/autoProcess.ini"
-ln -s "/var/log/sickbeard_mp4_automator/index.log" "/config/sma/index.log"
+if [ ! -f "/config/sma/index.log" ]; then
+	ln -s  "/config/sma/index.log" /"var/log/sickbeard_mp4_automator/index.log"
+fi
 
 exit 0
