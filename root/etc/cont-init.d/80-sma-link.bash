18f9ccab9eae92bb70a8b0462a9735996a2fee5a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-11 14:26:37 -0400

    Update 80-sma-link.bash

--- root/etc/cont-init.d/80-sma-link.bash
+++ root/etc/cont-init.d/80-sma-link.bash
@@ -1,35 +1,38 @@
 #!/usr/bin/with-contenv bash
 
+# create config directory
 if [ ! -d "/config/sma" ]; then
 	mkdir -p "/config/sma" && \
 	chmod 0777 -R "/config/sma"
 fi
 
+# import new config, if does not exist
 if [ ! -f "/config/sma/config/autoProcess.ini" ]; then
 	cp "/usr/local/sma/config/autoProcess.ini.sample" "/config/sma/autoProcess.ini" && \
-	chmod 0666 "/config/sma"/*  && \
-	sed -i "s/codec = aac/codec = libfdk_aac/g" "/config/sma/autoProcess.ini" && \
-	sed -i "s/ac3/eac3,ac3,aac,mp3/g" "/config/sma/autoProcess.ini" && \
-	sed -i "s/poster/thumb/g" "/config/sma/autoProcess.ini" && \
-	sed -i "s/languages = /languages = eng/g" "/config/sma/autoProcess.ini" && \
-	sed -i "s/default-language = /default-language = eng/g" "/config/sma/autoProcess.ini" && \
-	sed -i "s/burn-subtitles = False/burn-subtitles = forced/g" "/config/sma/autoProcess.ini" && \
-	sed -i "s/sort-streams = False/sort-streams = True/g" "/config/sma/autoProcess.ini"
 fi
 
+# link config file for use
 if [ ! -f "/usr/local/sma/config/autoProcess.ini" ]; then
 	ln -s "/config/sma/autoProcess.ini" "/usr/local/sma/config/autoProcess.ini"
 fi
 
+# remove sickbeard_mp4_automator log if exists
 if [ -f "/var/log/sickbeard_mp4_automator/index.log" ]; then
 	rm "/var/log/sickbeard_mp4_automator/index.log"
 fi
 
+# remove sickbeard_mp4_automator log from sma config folder if exists
 if [ -f "/config/sma/index.log" ]; then
 	rm "/config/sma/index.log"
 fi
 
-touch "/config/sma/index.log"
-ln -s "/config/sma/index.log" "/var/log/sickbeard_mp4_automator/index.log"
+# create sma log file
+touch "/config/sma/index.log" && \
+
+# link sma log file
+ln -s "/config/sma/index.log" "/var/log/sickbeard_mp4_automator/index.log" && \
+
+# set permissions
+chmod 0666 "/config/sma"/*
 
 exit 0
