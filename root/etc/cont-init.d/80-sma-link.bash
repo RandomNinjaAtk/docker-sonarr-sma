a15d332bd78a97db14029f273f31edc48532dd55
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-11 06:42:40 -0400

    Update 80-sma-link.bash

--- root/etc/cont-init.d/80-sma-link.bash
+++ root/etc/cont-init.d/80-sma-link.bash
@@ -1,17 +1,17 @@
 #!/usr/bin/with-contenv bash
 
 if [ ! -d "/config/sma/config" ]; then
-	mkdir -p "/config/sma/config" && \
-	chmod 0777 -R "/config/sma/config"
+	mkdir -p "/config/sma" && \
+	chmod 0777 -R "/config/sma"
 fi
 
 if [ ! -f "/config/sma/config/autoProcess.ini" ]; then
-	cp "/usr/local/sma/config/autoProcess.ini.sample" "/config/sma/config/autoProcess.ini" && \
-	chmod 0666 "/config/sma/config"/*
+	cp "/usr/local/sma/config/autoProcess.ini.sample" "/config/sma/autoProcess.ini" && \
+	chmod 0666 "/config/sma"/*
 fi
 
 if [ ! -f "/usr/local/sma/config/autoProcess.ini" ]; then
-	ln -s "/config/sma/config/autoProcess.ini" "/usr/local/sma/config/autoProcess.ini"
+	ln -s "/config/sma/autoProcess.ini" "/usr/local/sma/config/autoProcess.ini"
 fi
 
 if [ ! -f "/config/sma/index.log" ]; then
