0fcbdbe6763c8bd74e75d8e986b2bf535f684070
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-22 10:59:19 -0400

    Update 32-sma-link.bash

--- root/etc/cont-init.d/32-sma-link.bash
+++ root/etc/cont-init.d/32-sma-link.bash
@@ -1,28 +1,26 @@
 #!/usr/bin/with-contenv bash
 
+# Remove exisitng
+if [ -d "/config/sma" ]; then
+	rm -rf "/config/sma" && \
+	sleep 0.1
+fi
+
+if [ -d "/usr/local/sma/config" ]; then
+	rm -rf /usr/local/sma/config/* && \
+	sleep 0.1
+fi
+
 # create config directory
 if [ ! -d "/config/sma" ]; then
 	mkdir -p "/config/sma" && \
 	chmod 0777 -R "/config/sma"
 fi
 
-# delete existing config
-if [ -f "/usr/local/sma/config/autoProcess.ini" ]; then
-	rm "/usr/local/sma/config/autoProcess.ini"
-fi
 
 # import new config, if does not exist
 if [ ! -f "/config/sma/autoProcess.ini" ]; then
 	cp "/usr/local/sma/setup/autoProcess.ini.sample" "/usr/local/sma/config/autoProcess.ini"
-fi
-
-# remove sickbeard_mp4_automator log if exists
-if [ -f "/config/sma/sma.log" ]; then
-	rm "/config/sma/sma.log"
-fi
-
-if [ -f "/usr/local/sma/config/sma.log" ]; then
-	rm "/usr/local/sma/config/sma.log"
 fi
 
 # create sma log file
