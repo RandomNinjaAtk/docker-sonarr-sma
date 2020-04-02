6b634c470ea686e62d4784974729fd38663007a7
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-02 09:01:34 -0400

    New options for script

--- root/scripts/update.py
+++ root/scripts/update.py
@@ -44,6 +44,14 @@ def main():
     safeConfigParser.set("Converter", "ffprobe", "ffprobe")
     if os.environ.get("CONVERTER_THREADS"):
         safeConfigParser.set("Converter", "threads", os.environ.get("CONVERTER_THREADS"))
+    if os.environ.get("CONVERTER_HWACCELS"):
+        safeConfigParser.set("Converter", "hwaccels", os.environ.get("CONVERTER_HWACCELS"))
+    else:
+        safeConfigParser.set("Converter", "hwaccels", " ")
+    if os.environ.get("CONVERTER_HWACCEL_DECODERS"):
+        safeConfigParser.set("Converter", "hwaccel-decoders", os.environ.get("CONVERTER_HWACCEL_DECODERS"))
+    else:
+        safeConfigParser.set("Converter", "hwaccel-decoders", " ")
     if os.environ.get("CONVERTER_OUTPUT_FORMAT"):    
         safeConfigParser.set("Converter", "output-format", os.environ.get("CONVERTER_OUTPUT_FORMAT"))
     if os.environ.get("CONVERTER_OUTPUT_EXTENSION"):    
