2bcb6e9d000fd655710f69a0ad27325985b65082
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-07-19 11:40:02 -0400

    New MKVTagger Script

---
+++ root/etc/cont-init.d/33-script-setup.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bash
+
+# Create Scripts Directory
+if [ ! -d "/config/scripts" ]; then
+	mkdir "/config/scripts"
+fi
+
+# Remove existing script
+if [ -f "/config/scripts/MKVTagger.bash" ]; then
+	rm "/config/scripts/MKVTagger.bash"
+fi
+
+# import script
+if [ ! -f "/config/scripts/MKVTagger.bash" ]; then
+	cp "/scripts/MKVTagger.bash" "/config/scripts/MKVTagger.bash"
+fi
+
+# set permissions
+chmod 0777 -R "/config/scripts"
+
+exit $?
