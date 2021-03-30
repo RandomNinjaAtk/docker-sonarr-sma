a604f78db07366eb72facb24eb31900c990d1cdd
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2021-03-30 19:30:32 -0400

    Create 99-start.bash

---
+++ root/etc/cont-init.d/99-start.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bash
+echo "------------------------------------------------------------"
+echo "|~) _ ._  _| _ ._ _ |\ |o._  o _ |~|_|_|"
+echo "|~\(_|| |(_|(_)| | || \||| |_|(_||~| | |<"
+echo "Presenets: sonarr-sma"
+echo "------------------------------------------------------------"
+echo "Donate: https://github.com/sponsors/RandomNinjaAtk"
+echo "Project: https://github.com/RandomNinjaAtk/docker-sonarr-sma"
+echo "Support: https://discord.gg/JumQXDc"
+echo "------------------------------------------------------------"
+exit 0
